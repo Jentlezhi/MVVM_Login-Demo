e80991ddbd1b71fdce50296b093cb1f26f1dbea9
jentlezhi <juntaozhi@163.com>
2017-02-19 09:13:19 +0800

    add .gitignore

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'RAC' do
+  pod 'ReactiveCocoa','~> 2.5'
+end
---
+++ RAC/MBProgressHUD/MBProgressHUD+Extension.h
@@ -0,0 +1,25 @@
+//
+//  MBProgressHUD+Extension.h
+//
+//  Created by Jentle on 15-6-6.
+//  Copyright (c) 2015年 Jentle. All rights reserved.
+//
+
+#import "MBProgressHUD.h"
+
+@interface MBProgressHUD (Extension)
++ (void)showSuccess:(NSString *)success toView:(UIView *)view;
++ (void)showError:(NSString *)error toView:(UIView *)view;
+
++ (MBProgressHUD *)showMessage:(NSString *)message toView:(UIView *)view;
+
+
++ (void)showSuccess:(NSString *)success;
++ (void)showError:(NSString *)error;
+
++ (MBProgressHUD *)showMessage:(NSString *)message;
+
++ (void)hideHUDForView:(UIView *)view;
++ (void)hideHUD;
+
+@end
